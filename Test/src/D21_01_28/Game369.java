37fd489052ba8466f01ce61f78f40849b06b181e
구재경 <reimex111@gmail.com>
2021-01-28 12:32:24 +0900

    21.01.28 Game369 re

--- Test/src/D21_01_28/Game369.java
+++ Test/src/D21_01_28/Game369.java
@@ -8,13 +8,17 @@ public class Game369 {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("ÀÔ·Â : ");
-		int a = sc.nextInt();			//ÀÔ·Â¹Þ±â
-//		int a = (int)(Math.random() * 100) + 1;		//·£´ý°ª ¹Þ±â
 		
-		if(a % 3 == 0 && a != 0) {
+//		int a = (int)(Math.random() * 100) + 1;		//·£´ý°ª ¹Þ±â
+		int a = sc.nextInt();			//ÀÔ·Â¹Þ±â
+		
+		int b = a % 10;		//ÀÏÀÇ ÀÚ¸® 3, 6, 9 »Ì±â
+		int c = a / 10;		//½ÊÀÇ ÀÚ¸® 3, 6, 9 »Ì±â
+		
+		if(b % 3 == 0 && a != 0) {
 			System.out.print("Â¦");
-			if(a > 10 && (a / 10) % 3 == 0) {
-				System.out.print("Â¦");
+			if(c % 3 == 0 && a > 10) {
+				System.out.print("Â¦");				
 			}
 		} else {
 			System.out.println("¶¯");
